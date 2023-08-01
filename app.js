f056305d696c9c7db7a27af88e56ab210398e39d
Shradha Bhandare <Shradha.Bhandare@lntinfotech.com>
2023-08-01 15:59:40 +0530

    get api for blog list

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ const fs = require('fs');
 const app = express();
 
 const port = 4000;
+const blogs = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/blog.json`));
+
+app.get('/api/v1/blogs', (req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        length: blogs.length,
+        data: {
+            blogs
+        }
+    })
+})
+
+
+
 
 //creating server
 app.listen(port, ()=>{
-    console.log('From port 3000')
+    console.log('From port 3000', blogs)
 })
+
