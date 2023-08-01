086ce6e25ef91d95c897f8876b016d766943a0aa
Shradha Bhandare <Shradha.Bhandare@lntinfotech.com>
2023-08-01 16:32:02 +0530

    get api for single blog

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 4000;
 const blogs = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/blog.json`));
 
+// fetch list of blogs
 app.get('/api/v1/blogs', (req,res)=>{
     res.status(200).json({
         status: 'success',
@@ -16,11 +17,25 @@ app.get('/api/v1/blogs', (req,res)=>{
     })
 })
 
+// fetch single blog
+app.get('/api/v1/blogs/:id', (req, res)=>{
+   
+    const id = req.params.id * 1;
+    const blog = blogs.find(el=>el.id === id);
+   
+    res.status(200).json({
+        status: 'success',
+        data: {
+            blog
+        }
+    })
+})
+
 
 
 
 //creating server
 app.listen(port, ()=>{
-    console.log('From port 3000', blogs)
+    console.log('From port 4000')
 })
 
