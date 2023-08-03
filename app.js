1d7d083d2132e03a17c8f542e5b36340aa9be03f
Shradha Bhandare <Shradha.Bhandare@lntinfotech.com>
2023-08-03 14:55:01 +0530

    Mounted routers using middleware

--- app.js
+++ app.js
@@ -2,24 +2,34 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
+
+// custom middleware
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toLocaleDateString();
+    console.log(req.requestTime)
+    next();
+})
 
 const port = 4000;
 const blogs = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/blog.json`));
 
+
+const blogRouter = express.Router();
 // fetch list of blogs
-app.get('/api/v1/blogs', (req,res)=>{
+const getAllBlogs = (req, res) =>{
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         length: blogs.length,
         data: {
             blogs
         }
     })
-})
+}
 
 // fetch single blog
-app.get('/api/v1/blogs/:id', (req, res)=>{
-   
+const getBlog = (req, res) =>{
     const id = req.params.id * 1;
     const blog = blogs.find(el=>el.id === id);
    
@@ -29,9 +39,19 @@ app.get('/api/v1/blogs/:id', (req, res)=>{
             blog
         }
     })
-})
+}
 
 
+// app.get('/api/v1/blogs', getAllBlogs);
+// app.get('/api/v1/blogs/:id', getBlog)
+app.use('/api/v1/blogs', blogRouter);
+blogRouter
+.route('/')
+.get(getAllBlogs);
+
+blogRouter
+.route('/:id')
+.get(getBlog);
 
 
 //creating server
